d1f5308df35476f945b0edd6c1c5b8ece0db6bfe
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2025-10-06 05:57:37 +0530

    Add Hello class with main method to print greeting message

---
+++ src/com/core/Basics/Hello.java
@@ -0,0 +1,7 @@
+package com.core.Basics;
+
+public class Hello {
+    static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
--- src/com/core/Hello.java
+++
@@ -1,7 +0,0 @@
-package com.core;
-
-public class Hello {
-    public static void main(String[] args) {
-       System.out.println("Hi, Welcome to the World of Java!"); 
-    }
-}
